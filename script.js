b3900e5b5d8191d7eab6210e4fbfcbcee654638e
mjrgeorge <mjrgeorge@gmail.com>
2020-07-31 10:35:43 +0600

    function added

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 //GENERATE PIN BUTTON HANDLER
-document.getElementById('generate_btn').addEventListener('click', function () {
+function pinGenerate() {
     let generatePin = Math.random() * 9000 + 1000;
     generatePin = Math.floor(generatePin);
-    generateDisplay = document.getElementById('generate_display').value = generatePin;
-})
+    document.getElementById('generate_display').value = generatePin;
+};
 
 function insert(num) {
-
     document.getElementById('calc_display').value += num;
-}
+};
 
 function backSpace() {
     let n = document.getElementById('calc_display').value;
     document.getElementById('calc_display').value = n.substr(0, n.length - 1);
-
-}
+};
 
 function clean() {
     document.getElementById('calc_display').value = '';
-}
+};
+
+
+function isMatched() {
+    let generatePin = document.getElementById('generate_display').value;
+    let displayPin = document.getElementById('calc_display').value;
+
+    if (generatePin == displayPin) {
+        return document.getElementById('matched').style.display = 'block';
+    } else {
+        return document.getElementById('mis_matched').style.display = 'block';
+    }
+};
