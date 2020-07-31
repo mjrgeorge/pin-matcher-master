555d75fd227f7af88b8a8bdf4686089dc096d89c
mjrgeorge <mjrgeorge@gmail.com>
2020-08-01 02:46:53 +0600

    assignment task completed

--- script.js
+++ script.js
@@ -19,15 +19,34 @@ function clean() {
 };
 
 //SUBMIT BUTTON HANDLER
+let misMatchedCount = 0;
+
 function isMatched() {
     let generatePin = document.getElementById('generate_display').value;
     let displayPin = document.getElementById('calc_display').value;
-
     if (generatePin == displayPin) {
         document.getElementById('matched').style.display = 'block';
         document.getElementById('mis_matched').style.display = 'none';
     } else {
         document.getElementById('matched').style.display = 'none';
         document.getElementById('mis_matched').style.display = 'block';
-    }
+        misMatchedCount++;
+        tryLeft();
+    };
+};
+
+function tryLeft() {
+    switch (misMatchedCount) {
+        case 1:
+            document.getElementById('action_left').innerText = '2 try left';
+            break;
+        case 2:
+            document.getElementById('action_left').innerText = '1 try left';
+            break;
+        default:
+            document.getElementById('action_left').style.display = 'none';
+            document.getElementById('calc_display').setAttribute("disabled", "");
+            document.getElementById('submit_btn').setAttribute("disabled", "");
+            break;
+    };
 };
