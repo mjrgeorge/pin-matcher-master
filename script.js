ab8445be1db91ebbf5b45a45518e879878c98135
mjrgeorge <mjrgeorge@gmail.com>
2020-08-01 03:27:08 +0600

    try left update

--- script.js
+++ script.js
@@ -10,14 +10,46 @@ function insert(num) {
 };
 
 function backSpace() {
-    let n = document.getElementById('calc_display').value;
-    document.getElementById('calc_display').value = n.substr(0, n.length - 1);
+    let num = document.getElementById('calc_display').value;
+    document.getElementById('calc_display').value = num.substring(0, num.length - 1);
 };
 
 function clean() {
     document.getElementById('calc_display').value = '';
 };
 
+// //SUBMIT BUTTON HANDLER
+// let misMatchedCount = 0;
+
+// function isMatched() {
+//     let generatePin = document.getElementById('generate_display').value;
+//     let displayPin = document.getElementById('calc_display').value;
+//     if (generatePin == displayPin) {
+//         document.getElementById('matched').style.display = 'block';
+//         document.getElementById('mis_matched').style.display = 'none';
+//     } else {
+//         document.getElementById('matched').style.display = 'none';
+//         document.getElementById('mis_matched').style.display = 'block';
+//         misMatchedCount++;
+//         tryLeft();
+//     };
+// };
+
+// function tryLeft() {
+//     switch (misMatchedCount) {
+//         case 1:
+//             document.getElementById('action_left').innerText = '2 try left';
+//             break;
+//         case 2:
+//             document.getElementById('action_left').innerText = '1 try left';
+//             break;
+//         default:
+//             document.getElementById('action_left').style.display = 'none';
+//             document.getElementById('calc_display').setAttribute("disabled", "");
+//             document.getElementById('submit_btn').setAttribute("disabled", "");
+//             break;
+//     };
+// };
 //SUBMIT BUTTON HANDLER
 let misMatchedCount = 0;
 
@@ -43,9 +75,11 @@ function tryLeft() {
         case 2:
             document.getElementById('action_left').innerText = '1 try left';
             break;
+        case 3:
+            document.getElementById('action_left').innerText = '0 try left';
+            break;
         default:
-            document.getElementById('action_left').style.display = 'none';
-            document.getElementById('calc_display').setAttribute("disabled", "");
+            document.getElementById('action_left').innerText = 'Sorry, you are not eligible';
             document.getElementById('submit_btn').setAttribute("disabled", "");
             break;
     };
